e1e2f9f034cab49153b6fbe0866c1c908cb8797f
Amber <gynteee@yahoo.co.uk>
2022-12-15 11:30:33 +0000

    nameSam_Chap6

--- nameSam.rb
+++ nameSam.rb
@@ -3,12 +3,12 @@
 #The user enters their name.
 #If the user's name begins with 'S', the program shouts the user's name back at them.
 #If the user's name begins with any other letter, the program just says 'Hi, ' plus their name.
+
 puts "Welcome! What's your name?"
 name = gets.chomp
 name.capitalize!
-first_letter = name[0]
 
-if first_letter == "S"
+if name.chr == "S"
   puts "#{name}!".upcase
 else 
   puts "Hi #{name}"
